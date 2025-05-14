4372fbca03ea5ff46d99df53e35a493e295df83b
aboba <pomidkil@gmail.com>
2025-05-15 02:14:34 +0300

    Homework

---
+++ BST/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace BST;
+
+public class Node(int data)
+{
+    public int Data = data;
+    public Node? Left;
+    public Node? Right;
+}
+
+public class BST
+{
+    public Node? root;
+
+    public bool Search(int target, Node? node = null)
+    {
+        if (node is null) return false;
+        else if (node.Data == target) return true;
+
+        if (node.Data > target) 
+            return Search(target, node.Left);
+        else 
+            return Search(target, node.Right);
+    }
+
+    public void Add(int data)
+    {
+        Add(data, root);
+    }
+
+    private Node Add(int data, Node? node = null)
+    {
+        if (node is null) 
+        {
+            node = new Node(data);
+            return node;
+        }
+        if (node.Data > data) node.Left = Add(data, node.Left);
+        if (node.Data <= data) node.Right = Add(data, node.Right);
+
+        return node;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        BST bST = new BST();
+        bST.Add(10);
+        Console.WriteLine();
+    }
+}
---
+++ TreeHomework/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TreeHomeork;
+
+class Program
+{
+    public static void Main()
+    {
+        Node root = new(1)
+        {
+            Left = new(1),
+            Right = new(1),
+        };
+        root.Left.Left = new Node(1);
+        root.Left.Right = new Node(1);
+        root.Right.Left = new Node(1);
+        root.Right.Right = new Node(1);
+        root.Left.Left.Left = new Node(1);
+        root.Left.Left.Left.Left = new Node(1);
+        root.Left.Left.Left.Left.Left = new Node(1);
+
+        Console.WriteLine(LongNode(root));
+    }
+
+    public static int SumNode(Node? node)
+    {
+        if (node is null)
+            return 0;
+        return node.Data + SumNode(node.Right) + SumNode(node.Left);
+    }
+
+    public static int LongNode(Node? node)
+    {
+        if (node is null)
+            return 0;
+        return 1 + Math.Max(LongNode(node.Right), LongNode(node.Left));
+    }
+}
+
+public class Node(int data)
+{
+    public int Data = data;
+    public Node? Left;
+    public Node? Right;
+}
